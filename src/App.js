15992d540d81e85f803b9695d7a4e47bd24ad8bc
TomcyT <tomcy48@gmail.com>
2022-07-02 20:31:19 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quotesData, setQuotesData] = useState("");
+
+  const getRandomQoute = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((quotes) => {
+        const randomQuote = Math.floor(Math.random() * quotes.length);
+        setQuotesData(quotes[randomQuote]);
+      });
+  };
+
+  useEffect(() => {
+    getRandomQoute();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{quotesData.text}</h2>
+      <p>{quotesData.author}</p>
+      <button onClick={getRandomQoute}>Next Quote</button>
     </div>
   );
 }
