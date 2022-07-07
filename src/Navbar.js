691633354fec0777173f0dc77607aa1ce42d4528
TomcyT <tomcy48@gmail.com>
2022-07-07 14:04:02 +0530

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="navbar__container">
       <div className="navbar__text">React Quotes App</div>
       <div className="navbar__gitHubLogo">
-        <a href="https://github.com/TomcyT/react-qoutes-app">
+        <a href="https://github.com/TomcyT/react-qoutes-app" target="_blank">
           <img
             src="https://img.icons8.com/ios-glyphs/344/github.png"
             alt="github Logo"
